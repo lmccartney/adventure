b07f4326cb7915fcc3ecda33fea09e32ac80e99e
Lennon McCartney <mccartney.photo@gmail.com>
2019-05-25 17:55:52 -0400

    checking some stuff in

--- adventure/core/tests.py
+++ adventure/core/tests.py
@@ -2,4 +2,4 @@ from django.test import TestCase
 
 class FirstTest(TestCase):
     def test_failure(self):
-        self.fail()
+        pass
--- adventure/magic/models.py
+++ adventure/magic/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    """
+    These are distinct cards that Wizards has created over the years. The union
+    of this model & the Set model in Print creates the actual printings of
+    the card. This added complexity is there to facilitate the tracking of
+    monetary values for each printing.
+    """
+    name = models.CharField(max_length=64, unique=True)
+
+class Set(models.Model):
+    """
+    These are the actual sets printed by Wizards.
+    """
+    name = models.CharField(max_length=64, unique=True)
+
+class Print(models.Model):
+    """
+    The distinct printing of a card in a set
+    """
+    card = models.ForeignKey('magic.Card', on_delete=models.CASCADE)
+    set = models.ForeignKey('magic.Set', on_delete=models.CASCADE)
+
