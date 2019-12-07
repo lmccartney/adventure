f5243ff59f64105704d795ee0a76e5c16f7cdfd9
lennon <mccartney.photo@gmail.com>
2019-12-06 20:10:03 -0500

    fixing settings files

--- adventure/settings/environments/ci.py
+++ adventure/settings/environments/ci.py
@@ -9,5 +9,3 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
-from adventure.settings.components.core import *
